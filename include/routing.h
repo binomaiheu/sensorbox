576a0bf380d08bb1e3dba444b6650628e28d50f4
Bino Maiheu <bino.maiheu@vito.be>
2018-12-27 23:08:40 +0100

    added simple rest api implementation and modified main.cpp to include sd card dumping and ntp... problem is... sketch is too large :s

---
+++ include/routing.h
@@ -0,0 +1,40 @@
+/*
+ * Rest API Server for Arduino
+ * 
+ * Filipe C - fcaldas@canal-plus.fr
+ */
+#include <EthernetClient.h>
+
+#include "constants.h"
+
+enum REQUEST_TYPE {
+  GET,
+  POST
+};
+
+class __request{
+public:
+  //route name is written as a char array
+  //String datatype was causing a memory leak
+  char routename[ROUTESIZE];
+  REQUEST_TYPE rtype;
+};
+
+
+class __route{
+
+public:
+  char routename[ROUTESIZE];
+  REQUEST_TYPE rtype;
+  void (*callback)(EthernetClient *client, char args[]);
+
+  __route(char rname[], REQUEST_TYPE req, 
+          void (*callbackf)(EthernetClient *client, char args[]) ){
+    callback = callbackf;
+    strcpy(routename,rname);
+    rtype = req;
+  }
+  __route(){}
+  
+};
+
