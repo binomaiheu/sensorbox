04d07db981def2349049dd790a086cb387c9a535
Bino Maiheu <bino.maiheu@vito.be>
2018-12-28 00:55:31 +0100

    replaced the thingspeak library by the core routines from the file, reducing the size of the sketch and simply making it work for simultaneous upload of 4 variables, with the official thingspeak library, it simply doesn't work

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,8 @@
 #include <Arduino.h>
 #include <Ethernet.h>
 #include <SPI.h>
-#include <ThingSpeak.h>
+
+#include "memcheck.h"
 
 // Include project libraries in the lib folder
 #include <strfun.h>
@@ -11,7 +12,6 @@
 
 String sds_info = "";
 float pm10, pm25;
-
 
 // ThingSpeak
 unsigned long tsChannelNum = 662834;
@@ -26,6 +26,52 @@ byte mac[] = {
 // Ethernet
 EthernetClient client;
 
+#define TS_VER "1.5"
+#define THINGSPEAK_URL "api.thingspeak.com"
+#define THINGSPEAK_PORT_NUMBER 80
+#define TS_USER_AGENT "tslib-arduino/" TS_VER " (arduino uno or mega)"
+#define TIMEOUT_MS_SERVERRESPONSE 5000  // Wait up to five seconds for server to respond
+
+#define OK_SUCCESS              200     // OK / Success
+#define ERR_BADAPIKEY           400     // Incorrect API key (or invalid ThingSpeak server address)
+#define ERR_BADURL              404     // Incorrect API key (or invalid ThingSpeak server address)
+#define ERR_OUT_OF_RANGE        -101    // Value is out of range or string is too long (> 255 bytes)
+#define ERR_INVALID_FIELD_NUM   -201    // Invalid field number specified
+#define ERR_SETFIELD_NOT_CALLED -210    // setField() was not called before writeFields()
+#define ERR_CONNECT_FAILED      -301    // Failed to connect to ThingSpeak
+#define ERR_UNEXPECTED_FAIL     -302    // Unexpected failure during write to ThingSpeak
+#define ERR_BAD_RESPONSE        -303    // Unable to parse response
+#define ERR_TIMEOUT             -304    // Timeout waiting for server to respond
+#define ERR_NOT_INSERTED        -401    // Point was not inserted (most probable cause is the rate limit of once every 15 seconds)
+
+int getHTTPResponse(String & response)
+{
+  long startWaitForResponseAt = millis();
+  while(client.available() == 0 && millis() - startWaitForResponseAt < TIMEOUT_MS_SERVERRESPONSE)
+  {
+    delay(100);
+  }
+  if(client.available() == 0) return ERR_TIMEOUT; // Didn't get server response in time
+	if(!client.find(const_cast<char *>("HTTP/1.1"))) return ERR_BAD_RESPONSE; // Couldn't parse response (didn't find HTTP/1.1)
+
+  int status = client.parseInt();
+  if(status != OK_SUCCESS) return status;		
+
+	if(!client.find(const_cast<char *>("\r\n"))) return ERR_BAD_RESPONSE;
+	if(!client.find(const_cast<char *>("\n\r\n"))) return ERR_BAD_RESPONSE;
+		
+	String tempString = client.readString();
+	response = tempString;
+
+	return status;
+};
+
+int abortWriteRaw()
+{
+  client.stop();
+  return ERR_UNEXPECTED_FAIL;
+}
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -33,6 +79,12 @@ void setup() {
   Serial.println( "Booting sensorbox..." ); 
   sds_info = sds.get_info();
   Serial.println( sds_info );
+  delay(2000);
+  sds.get_values( pm10, pm25 );
+  Serial.println( String("PM25: ") + Float2String(pm25));
+  Serial.println( String("PM10: ") + Float2String(pm10));
+
+  Serial.println( "Freemem: " + String(freeMemory()));
 
   // start the Ethernet connection:
   Serial.println("Init Ethernet with DHCP:");
@@ -47,8 +99,6 @@ void setup() {
     Serial.println(Ethernet.localIP());
   }
 
-  Serial.println("Initializing THingspeak API...");
-  ThingSpeak.begin( client );  
 }
 
 void loop() {
@@ -85,6 +135,7 @@ void loop() {
       //nothing happened
       break;
   }
+  Serial.println( "Freemem: " + String(freeMemory()));
 
   // put your main code here, to run repeatedly:
   sht.readSensor();
@@ -97,21 +148,44 @@ void loop() {
   Serial.println( String("PM25: ") + Float2String(pm25));
   Serial.println( String("PM10: ") + Float2String(pm10));
 
-  // ThinkSpeak, convert the fields to string, otherwise somethings
-  // seems not to work with seting more than 3 floats. When setting to string it does seem 
-  // to work and the ThingSpeak will still interpret the values correctly
-  ThingSpeak.setField(1, String(pm25));
-  ThingSpeak.setField(2, String(pm10));
-  //ThingSpeak.setField(3, String(t));
-  //ThingSpeak.setField(4, String(rh));
 
- // write to the ThingSpeak channel 
-  int x = ThingSpeak.writeFields(tsChannelNum, tsAPIKey);
-  if ( x == 200 ) {
-    Serial.println("OK");
-  } else {
-    Serial.println("HTTP Error:" + String(x));
+  // connect to thingspeak
+  if ( client.connect( THINGSPEAK_URL, THINGSPEAK_PORT_NUMBER ) ) {
+    String postMessage = String("field1=") + Float2String(pm25) 
+                       + String("&field2=") + Float2String(pm10)
+                       + String("&field3=") + Float2String(t)
+                       + String("&field4=") + Float2String(rh)
+                       + String("&headers=false");
+    
+    Serial.println( "POST:" + postMessage );
+
+    if (!client.print("POST /update HTTP/1.1\r\n")) abortWriteRaw();
+		if (!client.print("Host: api.thingspeak.com\r\n")) abortWriteRaw();
+		if (!client.print("Connection: close\r\n")) abortWriteRaw();
+		if (!client.print("User-Agent: ")) abortWriteRaw();
+		if (!client.print(TS_USER_AGENT)) abortWriteRaw();
+		if (!client.print("\r\n")) abortWriteRaw();
+		if (!client.print("X-THINGSPEAKAPIKEY: ")) abortWriteRaw();
+		if (!client.print(tsAPIKey)) abortWriteRaw();
+		if (!client.print("\r\n")) abortWriteRaw();
+		if (!client.print("Content-Type: application/x-www-form-urlencoded\r\n")) abortWriteRaw();
+		if (!client.print("Content-Length: ")) abortWriteRaw();
+		if (!client.print(postMessage.length())) abortWriteRaw();
+		if (!client.print("\r\n\r\n")) abortWriteRaw();
+		if (!client.print(postMessage)) abortWriteRaw();
+		
+    String entryIDText = String();
+		int status = getHTTPResponse(entryIDText);
+		if(status != OK_SUCCESS)
+		{
+			client.stop();
+      Serial.println("TS update failed... ");			
+		}
+		long entryID = entryIDText.toInt();
+		Serial.print("Inserted Entry ID \"");Serial.print(entryIDText);Serial.print("\" (");Serial.print(entryID);Serial.println(")");
+		client.stop();
   }
+
 
   // thingspeak only allows updates everey 15 sec
   // so do this every 30
