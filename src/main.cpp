3f3d6df7c8f86056ea59f764dec5bdc2e49a6747
Bino Maiheu <bino.maiheu@vito.be>
2018-12-27 23:08:54 +0100

    added simple rest api implementation and modified main.cpp to include sd card dumping and ntp... problem is... sketch is too large :s

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,13 @@
 #include <Arduino.h>
 #include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <NTPClient.h>
 #include <SPI.h>
-#include <ThingSpeak.h>
+//#include <SD.h>
+
+#include "restapi.h"
+
+//#include <ThingSpeak.h>
 
 // Include project libraries in the lib folder
 #include <strfun.h>
@@ -12,10 +18,15 @@
 String sds_info = "";
 float pm10, pm25;
 
+// SD Card file
+/*
+const int sdChipSelect = 4;
+File dataFile;
+*/
 
 // ThingSpeak
-unsigned long tsChannelNum = 662834;
-const char*   tsAPIKey     = "60LARLDF6SNAEDV0";
+//unsigned long tsChannelNum = 662834;
+//const char*   tsAPIKey     = "60LARLDF6SNAEDV0";
 
 // Enter a MAC address for your controller below.
 // Newer Ethernet shields have a MAC address printed on a sticker on the shield
@@ -26,65 +37,63 @@ byte mac[] = {
 // Ethernet
 EthernetClient client;
 
+// NTP client
+EthernetUDP Udp;
+NTPClient timeClient(Udp, "pool.ntp.org", 0, 300000 ); // update every 5 minutes
+
+
+// callback functions
+restServer api( 80 );
+void displayIndex( EthernetClient* client, char *args )
+{
+  client->println( "<html><head></head><body>Hello from arduino</body></html>" ); 
+  return;
+}
+
+
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   delay(1000);
-  Serial.println( "Booting sensorbox..." ); 
-  sds_info = sds.get_info();
-  Serial.println( sds_info );
+  Serial.println( "Booting..." ); 
+  //sds_info = sds.get_info();
+ // Serial.println( sds_info );
 
   // start the Ethernet connection:
-  Serial.println("Init Ethernet with DHCP:");
+  Serial.print("Init DHCP : ");
   if (Ethernet.begin(mac) == 0) {
-    Serial.println("Failed to configure Ethernet using DHCP");   
-    // no point in carrying on, so do nothing forevermore:
+    Serial.println("failed");       
     while (true) {
       delay(1);
     }
   } else {
-    Serial.print("  DHCP assigned IP ");
-    Serial.println(Ethernet.localIP());
+    Serial.println(String("success: ")+String(Ethernet.localIP()));    
   }
 
+  Serial.print("Init NTP:");
+  timeClient.begin();
+
+/*
   Serial.println("Initializing THingspeak API...");
   ThingSpeak.begin( client );  
+*/
+
+  
+  Serial.print("Init SD card : ");
+  if ( !SD.begin( sdChipSelect ) ) { 
+    Serial.println( "failed." );
+    while (1);
+  }
+  Serial.println( "done." );
+
+  // init rest api
+  api.addRoute( "/index.html", GET, &displayIndex );  
+
 }
 
 void loop() {
 
-  // check DHCP 
-  switch (Ethernet.maintain()) {
-    case 1:
-      //renewed fail
-      Serial.println("Error: renewed fail");
-      break;
-
-    case 2:
-      //renewed success
-      Serial.println("Renewed success");
-      //print your local IP address:
-      Serial.print("My IP address: ");
-      Serial.println(Ethernet.localIP());
-      break;
-
-    case 3:
-      //rebind fail
-      Serial.println("Error: rebind fail");
-      break;
-
-    case 4:
-      //rebind success
-      Serial.println("Rebind success");
-      //print your local IP address:
-      Serial.print("My IP address: ");
-      Serial.println(Ethernet.localIP());
-      break;
-
-    default:
-      //nothing happened
-      break;
-  }
+  // timeClient.update();
 
   // put your main code here, to run repeatedly:
   sht.readSensor();
@@ -97,6 +106,44 @@ void loop() {
   Serial.println( String("PM25: ") + Float2String(pm25));
   Serial.println( String("PM10: ") + Float2String(pm10));
 
+  // get timestamp after read
+  //unsigned long timestamp = timeClient.getEpochTime();
+
+  // serving the api...
+  api.serve();
+
+  // check DHCP 
+  switch (Ethernet.maintain()) {
+    case 1:
+      //renewed fail
+      Serial.println("Error: renewed fail");
+      break;
+
+    case 2:
+      //renewed success
+      Serial.println("Renewed success");
+      //print your local IP address:
+      Serial.println(String("My IP address: ") + String(Ethernet.localIP()));
+      break;
+
+    case 3:
+      //rebind fail
+      Serial.println("Error: rebind fail");
+      break;
+
+    case 4:
+      //rebind success
+      Serial.println("Rebind success");
+      //print your local IP address:
+      Serial.println(String("My IP address: ") + String(Ethernet.localIP()));
+      break;
+
+    default:
+      //nothing happened
+      break;
+  }
+
+/*
   // ThinkSpeak, convert the fields to string, otherwise somethings
   // seems not to work with seting more than 3 floats. When setting to string it does seem 
   // to work and the ThingSpeak will still interpret the values correctly
@@ -112,6 +159,18 @@ void loop() {
   } else {
     Serial.println("HTTP Error:" + String(x));
   }
+  */
+
+/*
+  File dataFile = SD.open( "datalog.txt", FILE_WRITE );
+  if (dataFile){
+    dataFile.println( String(timestamp) + "\t" + Float2String(t) + "\t" + Float2String(rh) + "\t" + Float2String(pm25) + "\t" + Float2String(pm10) );
+    //dataFile.println( Float2String(t) + "\t" + Float2String(rh) + "\t" + Float2String(pm25) + "\t" + Float2String(pm10) );
+    dataFile.close();
+  } else {
+    Serial.println( "error opening datafile..." );
+  }
+*/
 
   // thingspeak only allows updates everey 15 sec
   // so do this every 30
