4c531d77120af3c3b9c35d8e379cbc458338e1f8
Bino Maiheu <bino.maiheu@vito.be>
2018-12-28 18:23:11 +0100

    removed String dependency as it causes irratic behaviour and replaced by low level string functions

--- lib/strfun/strfun.h
+++ lib/strfun/strfun.h
@@ -4,3 +4,7 @@
 
 String Float2String(const double value);
 String Float2String(const double value, uint8_t digits);
+
+char *ltrim(char *str, const char *seps);
+char *rtrim(char *str, const char *seps);
+char *trim(char *str, const char *seps);
